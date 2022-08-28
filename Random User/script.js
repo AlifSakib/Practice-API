a3362ceea788de0acd5d62929a35ded27234f325
AlifSakib <alifsakib@gmail.com>
2022-08-29 02:34:11 +0600

    added onClick info change

--- Random User/script.js
+++ Random User/script.js
@@ -5,7 +5,52 @@ const loadUser = async () => {
 };
 
 const displayUser = (users) => {
-  console.log(users);
+  const userContainer = document.getElementById("user-container");
+
+  users.forEach((user) => {
+    const { title, first, last } = user.name;
+    const { medium } = user.picture;
+    const {
+      street: { number, name },
+    } = user.location;
+    const { date } = user.dob;
+    const { password } = user.login;
+    const { phone } = user;
+    const userDiv = document.createElement("div");
+    userDiv.classList.add("user");
+    userDiv.innerHTML = `
+            <img src="${medium}" alt="" />
+            <p>Hi, My Name is</p>
+            <h3 id="info" >${title} ${first} ${last}</h3>
+            <div id="icons">
+            <i class="fa-solid fa-address-card"></i>
+            <i id="email" class="fa-solid fa-message"></i>
+            <i id="dob" class="fa-solid fa-calendar"></i>
+            <i id="location" class="fa-solid fa-location-pin"></i>
+            <i id="phone" class="fa-solid fa-phone"></i>
+            <i id="password" class="fa-solid fa-lock"></i>
+            </div>
+    `;
+    userContainer.appendChild(userDiv);
+    const info = document.getElementById("info");
+    const icons = document
+      .getElementById("icons")
+      .addEventListener("mouseover", function (e) {
+        if (e.target.id === "location") {
+          info.innerText = name;
+        } else if (e.target.id === "dob") {
+          info.innerText = date.slice(0, 10);
+        } else if (e.target.id === "email") {
+          info.innerText = user.email;
+        } else if (e.target.id === "phone") {
+          info.innerText = phone;
+        } else if (e.target.id === "password") {
+          info.innerText = password;
+        } else {
+          info.innerText = title + " " + first + " " + last;
+        }
+      });
+  });
 };
 
 loadUser();
